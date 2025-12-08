1c07d0310533a37680d098f3f8d67e71f32c01d5
Bence Dallos <dallos.bence@gmail.com>
2025-12-08 13:28:58 +0100

    Use firstname everywhere

--- src/utils/registryHelper.ts
+++ src/utils/registryHelper.ts
@@ -10,7 +10,7 @@ export const getUserMap = (entries: RegistryEntryView[]) => {
   const userMap: UserMap = {};
   entries.forEach((entry) => {
     const userId = entry.userId;
-    if (!userMap[userId]) userMap[userId] = {name: entry.userName, points: 0, avatar: entry.userPhotoURL};
+    if (!userMap[userId]) userMap[userId] = {name: entry.userName.split(' ')[0], points: 0, avatar: entry.userPhotoURL};
     userMap[userId].points += entry.points
   })
   return userMap;
